5b56b4237ff3612a467fb5c55ade08b4854177ab
user-123-456 <milagrospalmasaenz@gmail.com>
2019-11-07 13:26:08 -0500

    add functions medical appointment

---
+++ js/hch2019.js
@@ -0,0 +1,156 @@
+var data = {
+	"especialidad": [
+		{
+			"id": "E001",
+			"name": "Cardiología"
+		},
+		{
+			"id": "E002",
+			"name": "Medicina General"
+		},
+		{
+			"id": "E003",
+			"name": "Nutrición"
+		},
+		{
+			"id": "E004",
+			"name": "Psicología"
+		},
+		{
+			"id": "E005",
+			"name": "Traumatología"
+		}
+	],
+	"consultorio": [
+		{
+			"id": "001",
+			"description": "CRON1"
+		},
+		{
+			"id": "002",
+			"description": "CRON2"
+		},
+		{
+			"id": "003",
+			"description": "CRON3"
+		}
+	],
+	"horario": [
+		{
+			"id": "001",
+			"order": "5/10",
+			"namedoc": "Astrid Matos",
+			"cmp": "027424",
+			"spec_id": "E002",
+			"date": "12/12/19",
+			"hour": "10:30",
+			"cons": "CRON1"
+		},
+		{
+			"id": "002",
+			"order": "7/10",
+			"namedoc": "Astrid Matos",
+			"cmp": "027424",
+			"spec_id": "E002",
+			"date": "12/12/19",
+			"hour": "11:30",
+			"cons": "CRON1"
+		},
+		{
+			"id": "003",
+			"order": "8/10",
+			"namedoc": "Astrid Matos",
+			"cmp": "027424",
+			"spec_id": "E002",
+			"date": "12/12/19",
+			"hour": "12:00",
+			"cons": "CRON1"
+		},
+		{
+			"id": "004",
+			"order": "2/15",
+			"namedoc": "Alfredo Solar",
+			"cmp": "034812",
+			"spec_id": "E002",
+			"date": "13/12/19",
+			"hour": "08:30",
+			"cons": "CRON2"
+		},
+		{
+			"id": "005",
+			"order": "2/10",
+			"namedoc": "Astrid Matos",
+			"cmp": "027424",
+			"spec_id": "E002",
+			"date": "13/12/19",
+			"hour": "08:30",
+			"cons": "CRON1"
+		},
+		{
+			"id": "006",
+			"order": "6/10",
+			"namedoc": "Maria Pérez",
+			"cmp": "025063",
+			"spec_id": "E002",
+			"date": "15/12/19",
+			"hour": "11:30",
+			"cons": "CRON2"
+		}
+	],
+	"medicos": [
+		{
+			"cmp": "025063",
+			"firstname": "Maria",
+			"lastname": "Pérez Fernandez",
+			"spec_id": "E002"
+		},
+		{
+			"cmp": "034812",
+			"firstname": "Alfredo",
+			"lastname": "Solar Gonzalez",
+			"spec_id": "E002"
+		},
+		{
+			"cmp": "027424",
+			"firstname": "Astrid",
+			"lastname": "Matos Flores",
+			"spec_id": "E002"
+		},
+		{
+			"cmp": "061723",
+			"firstname": "Walter",
+			"lastname": "Abramonte Tene",
+			"spec_id": "E001"
+		},
+		{
+			"cmp": "021691",
+			"firstname": "Pedro",
+			"lastname": "Acosta Ramos",
+			"spec_id": "E001"
+		},
+		{
+			"cmp": "059064",
+			"firstname": "Melissa",
+			"lastname": "Rodriguez Lima",
+			"spec_id": "E001"
+		},
+	],
+	"pacientes": [
+		{
+			"doc": "72962622",
+			"name": "Juan Alberto",
+			"lastname": "Diaz Ramos",
+			"gender": "Masculino",
+			"nhc": "707172",
+			"sis": "035187045123"
+		},
+		{
+			"doc": "06894196",
+			"name": "Maria Mercedes",
+			"lastname": "Solis Tello",
+			"gender": "Femenino",
+			"nhc": "707075",
+			"sis": ""
+		}
+	]
+}
--- js/main.js
+++ js/main.js
@@ -14,6 +14,11 @@
             $containerClinicHistory.addClass( 'd-none' );
             $containerProofPayment.addClass( 'd-none' );
             $containerMedicalAppointments.removeClass( 'd-none' );
+
+            //cargando especialidades -> json
+            $.each(data.especialidad, function(i, spec) {
+                $('#selectSpec').append('<option value=' + i + ' data-id="' + spec.id + '">' + spec.name + '</option>');
+            });
         });
 
         $( 'body' ).on( 'click', '#btnClinicHistory', function(e) {
@@ -35,6 +40,110 @@
             $containerClinicHistory.addClass( 'd-none' );
             $containerProofPayment.addClass( 'd-none' );
             $containerMainMenu.removeClass( 'd-none' );
+
+            //Salir - generar cita
+            $('#selectSpec').empty();
+            $('#selectSpec').append('<option selected>Seleccionar especialidad...</option>');
+        });
+
+        /*----------------------------------GENERAR CITA--------------------------------------*/
+        //buscar paciente
+        $containerMedicalAppointments.on( 'click', '.btn-patient', function() {
+            var value = $containerMedicalAppointments.find( '#inputDni' ).val();
+            if ( value.length === 8 ) {
+                $.each( data.pacientes, function(i) {
+                    patient = data.pacientes[i];
+                    if(value===patient.doc) {
+                        $containerMedicalAppointments.find('#inputName').val(patient.name);
+                        $containerMedicalAppointments.find('#inputLastName').val(patient.lastname);
+                        $containerMedicalAppointments.find('#inputHC').val(patient.nhc);
+                        $containerMedicalAppointments.find('#inputGender').val(patient.gender);
+                        $containerMedicalAppointments.find('#inputSis').val('');
+                    }         
+                });
+            }
+            if (value.length === 0 || value.length < 8) {
+                alert('Ingrese documento de identidad válido.');
+            }
+            
+        });
+
+        //buscar sis
+        $containerMedicalAppointments.on( 'click', '.btn-sis', function() {
+            var value = $containerMedicalAppointments.find( '#inputDni' ).val();
+            if ( value.length <= 8 ) {
+                $.each( data.pacientes, function(i) {
+                    patient = data.pacientes[i];
+                    if(value===patient.doc) {
+                        if(patient.sis !== "") {
+                            $containerMedicalAppointments.find('#inputSis').val(patient.sis);
+                        } else {
+                            $containerMedicalAppointments.find('#inputSis').val("######");                            
+                        }
+                    }                
+                });
+            }
+            if (value.length === 0 || value.length < 8) {
+                alert('Ingrese documento de identidad válido.');
+            }
+        });
+
+        //combobox especialidad
+        $containerMedicalAppointments.on( 'change', '#selectSpec', function() {
+            var specSelected = $("#selectSpec option:selected").attr('data-id');
+            $containerMedicalAppointments.find('tbody .timetable-row').remove();
+            $('#inputDate, #inputHour, #inputAppoint').val('');
+            if ( typeof(specSelected) !== typeof undefined) {
+                $('#selectDoc').removeAttr('disabled');
+                $('#selectDoc').empty();
+                $('#selectDoc').append('<option selected>Seleccionar médico...</option>');
+                $.each(data.medicos, function(i, med) {
+                    if ( med.spec_id === specSelected ) {
+                        $('#selectDoc').append('<option value=' + i + ' data-cmp="' + med.cmp + '">' + med.firstname + ' ' + med.lastname + '</option>');
+                    }
+                });
+
+                //cargando horarios de especialidad
+                $.each(data.horario, function(i, row) {
+                    if ( row.spec_id === specSelected ) {
+                        $containerMedicalAppointments.find('tbody').append('<tr class="timetable-row"><td class="order">'+row.order+'</td><td class="doctor" data-cmp="'+row.cmp+'">'+row.namedoc+'</td><td class="date">'+row.date+'</td><td class="hour">'+row.hour+'</td><td>'+row.cons+'</td></tr>')
+                    }
+                });
+
+            } else {
+                $('#selectDoc').attr('disabled', true);
+                $('#selectDoc').empty();
+                $('#selectDoc').append('<option selected>Seleccionar médico...</option>');
+            }
+        });
+
+        //grilla
+        function aleatorio(minimo,maximo){
+            return Math.round(Math.random() * (maximo - minimo) + minimo);
+        }
+
+        $containerMedicalAppointments.on( 'click', '.timetable-row', function() {
+            var cmpDoctor = $(this).find('.doctor').data('cmp'),
+                valDate = $(this).find('.date').text(),
+                valHour = $(this).find('.hour').text();
+            $.each( $('#selectDoc option'), function(i) {
+                var cmpSelect = $(this).data('cmp');
+                $(this).removeAttr('selected');
+                if (cmpSelect === cmpDoctor) {
+                    $(this).attr('selected', 'selected');
+                }
+            });
+            $('#inputDate').val(valDate);
+            $('#inputHour').val(valHour);
+            $('#inputAppoint').val(aleatorio(100000, 999999));
+        });
+
+        //Registrar cita
+        $containerMedicalAppointments.on( 'click', '#cma-register', function() {
+            var valAppoint = $('#inputAppoint').val();
+            if ($('#inputAppoint').val() !== '' && $('#inputDni').val() !== '') {
+                alert( 'Cita generada con el Nro. ' + valAppoint );
+            }
         });
 
         // Comprobante de Pago
